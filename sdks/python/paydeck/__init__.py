a86e5a2efbc0363d3779c167f32434bf46c6b9e9
codecraker <vikas.budde@hotmail.com>
2026-09-23 23:01:36 +0530

    chore(python): bump version to 1.0.1 and attribute Vikas Budde (codecraker) as author

--- sdks/python/paydeck/__init__.py
+++ sdks/python/paydeck/__init__.py
@@ -1,5 +1,5 @@
 from paydeck.client import PayDeckClient
 from paydeck.exceptions import PayDeckError, PayDeckAPIError
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["PayDeckClient", "PayDeckError", "PayDeckAPIError"]
